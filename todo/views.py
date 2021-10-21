c92ce31daffa02139de184cc538bb758f86672c9
pokacyb <pokacyb@tutanota.com>
2021-10-21 14:59:17 +0200

    Add views.py and modify urls.py

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 def todo_list(request):
-    return render(request, "todo/todo_list.html")
+    todos = Todo.objects.all()
+    print(todos)
+    context = {
+        "todo_list":todos
+    }
+    return render(request, "todo/todo_list.html", context)
