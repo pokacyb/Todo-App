c4cbc188669de45c0bc2be0cff7a3bc902685f21
pokacyb <pokacyb@tutanota.com>
2021-10-21 16:43:10 +0200

    Add todo_delete method

--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,8 @@ from .views import (
     todo_list, 
     todo_detail, 
     todo_create,
-    todo_update
+    todo_update,
+    todo_delete,
 )
 
 app_name = 'todos'
@@ -13,7 +14,9 @@ urlpatterns = [
     path('', todo_list),
     path('create/', todo_create),
     path('<id>/', todo_detail),
-    path('<id>/update', todo_update),
+    path('<id>/update/', todo_update),
+    path('<id>/delete/', todo_delete),
+
     
 
 ]
