26ff6eecf346521aa751fed5b2b28467464ceeb3
pokacyb <pokacyb@tutanota.com>
2021-10-22 16:23:25 +0200

    Add .gitignore

--- Todo_App/settings.py
+++ Todo_App/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cj^$@4)a(5__l=p26-buahe2jhiq=%v=fp0*b3$ezunaunl0%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
